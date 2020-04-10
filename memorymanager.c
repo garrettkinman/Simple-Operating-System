6b53dcebeab6025ede9f911c252bdde01feb27e0
garrettkinman <garrettkinman@gmail.com>
2020-04-10 16:38:15 -0400

    initial commit

---
+++ memorymanager.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "pcb.h"
+
+int launcher(FILE *p) {
+    // TODO
+}
+
+int countTotalPages(FILE *f) {
+    // TODO
+}
+
+void loadPage(int pageNumber, FILE *f, int frameNumber) {
+    // TODO
+}
+
+int findFrame() {
+    // TODO
+}
+
+int findVictim(PCB *p) {
+    // TODO
+}
+
+int updatePageTable(PCB *p, int pageNumber, int frameNumber, int victimFrame) {
+    // TODO
+}
