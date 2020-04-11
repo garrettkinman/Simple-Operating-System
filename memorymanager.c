8f81c69fe40cc5357a0891cd8b9476db97017415
garrettkinman <garrettkinman@gmail.com>
2020-04-10 21:38:37 -0400

    pcb modifications

--- memorymanager.c
+++ memorymanager.c
@@ -6,7 +6,7 @@
 */
 int launcher(FILE *p) {
     // TODO
-    // 1) Copy the entire file into the backing store.
+    // 1) Copy the entire file into the backing store. (set filename as the PID)
     // 2) Close the file pointer pointing to the original file.
     // 3) Open the file in the backing store.
     // 4) Our launch paging technique defaults to loading two pages of the program into RAM
@@ -14,6 +14,7 @@ int launcher(FILE *p) {
         // If the program has 4 or fewer lines of code, then only one page is loaded.
         // If the program has more than 8 lines of code, then only the first two pages are loaded.
         // To do this, use the below helper functions.
+    
 }
 
 /*
--- pcb.c
+++ pcb.c
@@ -5,12 +5,22 @@ PCB has 3 fields
 PC : Points to the the current line reached for that program
 start: Points to first line of that program
 end: Points to last line of that program
+
+~~~NEW STUFF~~~
+pageTable: index is the page number; values stored in each cell are the frame numbers
+PC_page: which page the process is currently on
+PC_offset: which line within the page the process is currently on
+pages_max: total number of pages within this process
 */
 typedef struct PCB
 {
     int PC;
     int start;
     int end;
+    int pageTable[10];
+    int PC_page;
+    int PC_offset;
+    int pages_max;
 }PCB;
 
 
